ac97a33931613d333ee91493234c22536ea5d24f
sarahpopeck <121987980+sarahpopeck@users.noreply.github.com>
2025-03-22 14:37:58 -0400

    Delete src/milvus.py

--- src/milvus.py
+++
@@ -1,6 +0,0 @@
-from pymilvus import connections
-
-# Connect to Milvus instance (default localhost and port)
-connections.connect(host='localhost', port='19530')
-
-print("Connected to Milvus successfully!")
